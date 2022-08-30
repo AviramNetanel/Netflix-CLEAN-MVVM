241010ba6cd2823669098551af4d3433a2184099
Zach Bazov <zachbazov@gmail.com>
2022-08-31 00:18:40 +0300

    Initial Commit

---
+++ netflix/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  netflix
+//
+//  Created by Zach Bazov on 31/08/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
